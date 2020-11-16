60381271c0461cca85282ca14647aa880e07be25
Uli Ahda Raihan <18102034@ittelkom-pwt.ac.id>
2020-11-16 13:39:37 +0700

    Finish RecyclerView List

--- Praktikum6/app/src/main/java/com/uliahdaraihan_18102034/praktikum6/MainActivity.kt
+++ Praktikum6/app/src/main/java/com/uliahdaraihan_18102034/praktikum6/MainActivity.kt
@@ -2,10 +2,38 @@ package com.uliahdaraihan_18102034.praktikum6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.uliahdaraihan_18102034.praktikum6.adapter.ListMyDataAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val list = ArrayList<MyData>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rv_mydata.setHasFixedSize(true)
+        list.addAll(getListMyDatas())
+        showRecyclerList()
+    }
+    fun getListMyDatas(): ArrayList<MyData> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.getStringArray(R.array.data_photo)
+        val listMyData = ArrayList<MyData>()
+        for (position in dataName.indices) {
+            val myData = MyData(
+                dataName[position],
+                dataDescription[position],
+                dataPhoto[position]
+            )
+            listMyData.add(myData)
+        }
+        return listMyData
+    }
+    private fun showRecyclerList() {
+        rv_mydata.layoutManager = LinearLayoutManager(this)
+        val listMyDataAdapter = ListMyDataAdapter(list)
+        rv_mydata.adapter = listMyDataAdapter
     }
 }
--- Praktikum6/app/src/main/java/com/uliahdaraihan_18102034/praktikum6/MyData.kt
+++ Praktikum6/app/src/main/java/com/uliahdaraihan_18102034/praktikum6/MyData.kt
@@ -3,11 +3,9 @@ package com.uliahdaraihan_18102034.praktikum6
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
-class MyData {
-    @Parcelize
-    data class MyData(
-        var name: String,
-        var description: String,
-        var photo: String
-    ) : Parcelable
-}
+@Parcelize
+data class MyData(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
