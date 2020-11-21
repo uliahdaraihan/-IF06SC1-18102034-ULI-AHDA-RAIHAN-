b3b23031ce8ea9394905436cf6cea939cfe02819
Uli Ahda Raihan <18102034@ittelkom-pwt.ac.id>
2020-11-22 06:42:38 +0700

    Completed praktikum 6 for praktikum 7

---
+++ Praktikum7/app/src/main/java/com/uliahdaraihan_18102034/praktikum7/MyData.kt
@@ -0,0 +1,11 @@
+package com.uliahdaraihan_18102034.praktikum7
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MyData(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
