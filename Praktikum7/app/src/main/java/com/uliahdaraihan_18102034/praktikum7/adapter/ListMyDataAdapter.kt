d1a5ce4176a958efc69b3eeea659a0e8173567c6
Uli Ahda Raihan <18102034@ittelkom-pwt.ac.id>
2020-11-22 11:33:13 +0700

    added extra steps to GridMyDataAdapter and ListMyDataAdapter

--- Praktikum7/app/src/main/java/com/uliahdaraihan_18102034/praktikum7/adapter/ListMyDataAdapter.kt
+++ Praktikum7/app/src/main/java/com/uliahdaraihan_18102034/praktikum7/adapter/ListMyDataAdapter.kt
@@ -1,23 +1,37 @@
 package com.uliahdaraihan_18102034.praktikum7.adapter
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.uliahdaraihan_18102034.praktikum7.DetailActivity
 import com.uliahdaraihan_18102034.praktikum7.MyData
 import com.uliahdaraihan_18102034.praktikum7.R
 import kotlinx.android.synthetic.main.item_list.view.*
 
-class ListMyDataAdapter(private val listMyData: ArrayList<MyData>) :
+class ListMyDataAdapter(private val listMyData: ArrayList<MyData>, val context: Context) :
     RecyclerView.Adapter<ListMyDataAdapter.ListViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
         return ListViewHolder(view)
     }
+
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val myData = listMyData[position]
+//        Glide.with(holder.itemView.context)
+//        .load(listMyData[position].photo)
+//        .apply(RequestOptions().override(350, 550))
+//        .into(holder.imgPhoto)
         holder.bind(listMyData[position])
+        holder.itemView.setOnClickListener {
+            val moveWithObjectIntent = Intent(context, DetailActivity::class.java)
+            moveWithObjectIntent.putExtra(DetailActivity.EXTRA_MYDATA, myData)
+            context.startActivity(moveWithObjectIntent)
+        }
     }
     override fun getItemCount(): Int = listMyData.size
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
