26f86488d74be1ab918496beb51b6208235f738c
Uli Ahda Raihan <18102034@ittelkom-pwt.ac.id>
2020-11-22 08:05:20 +0700

    Finish 7.2 Scrolling Activity

--- Praktikum7/app/src/main/java/com/uliahdaraihan_18102034/praktikum7/DetailActivity.kt
+++ Praktikum7/app/src/main/java/com/uliahdaraihan_18102034/praktikum7/DetailActivity.kt
@@ -1,21 +1,44 @@
 package com.uliahdaraihan_18102034.praktikum7
 
+import android.app.Activity
 import android.os.Bundle
+import android.os.Parcelable
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_scrolling.*
 
 class DetailActivity : AppCompatActivity() {
-
+    companion object {
+        const val EXTRA_MYDATA = "extra_mydata"
+    }
+    inline fun <reified T : Parcelable> Activity.getParcelableExtra(key: String) = lazy {
+        intent.getParcelableExtra<T>(key)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(findViewById(R.id.toolbar))
-        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
+        val myData by getParcelableExtra<MyData>(DetailActivity.EXTRA_MYDATA)
+        supportActionBar?.title = myData?.name.toString()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        tv_detail_description.text = myData?.description.toString()
+        Glide.with(this)
+            .load(myData?.photo.toString())
+            .apply(RequestOptions().override(700, 700))
+            .into(iv_detail_photo)
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- Praktikum7/app/src/main/java/com/uliahdaraihan_18102034/praktikum7/adapter/CardViewMyDataAdapter.kt
+++ Praktikum7/app/src/main/java/com/uliahdaraihan_18102034/praktikum7/adapter/CardViewMyDataAdapter.kt
@@ -1,5 +1,7 @@
 package com.uliahdaraihan_18102034.praktikum7.adapter
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,10 +12,11 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.uliahdaraihan_18102034.praktikum7.DetailActivity
 import com.uliahdaraihan_18102034.praktikum7.MyData
 import com.uliahdaraihan_18102034.praktikum7.R
 
-class CardViewMyDataAdapter(private val listMyDatas: ArrayList<MyData>) :
+class CardViewMyDataAdapter(private val listMyDatas: ArrayList<MyData>, val context: Context) :
     RecyclerView.Adapter<CardViewMyDataAdapter.CardViewViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewViewHolder {
         val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_cardview, parent, false)
@@ -31,8 +34,11 @@ class CardViewMyDataAdapter(private val listMyDatas: ArrayList<MyData>) :
                 listMyDatas[holder.adapterPosition].name, Toast.LENGTH_SHORT).show() }
         holder.btnShare.setOnClickListener { Toast.makeText(holder.itemView.context, "Share " +
                 listMyDatas[holder.adapterPosition].name, Toast.LENGTH_SHORT).show() }
-        holder.itemView.setOnClickListener { Toast.makeText(holder.itemView.context, "Kamu memilih " +
-                listMyDatas[holder.adapterPosition].name, Toast.LENGTH_SHORT).show() }
+        holder.itemView.setOnClickListener {
+            val moveWithObjectIntent = Intent(context, DetailActivity::class.java)
+            moveWithObjectIntent.putExtra(DetailActivity.EXTRA_MYDATA, myData)
+            context.startActivity(moveWithObjectIntent)
+        }
     }
     override fun getItemCount(): Int {
         return listMyDatas.size
