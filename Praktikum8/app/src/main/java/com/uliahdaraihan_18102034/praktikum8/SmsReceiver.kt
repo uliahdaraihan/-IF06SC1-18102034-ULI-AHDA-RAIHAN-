ed5b9b0aa28889187cfa5d7ee1266bd6010dc178
Uli Ahda Raihan <18102034@ittelkom-pwt.ac.id>
2020-12-13 14:23:09 +0700

    Permission at MainActivity.kt Add Sms Receiver

---
+++ Praktikum8/app/src/main/java/com/uliahdaraihan_18102034/praktikum8/SmsReceiver.kt
@@ -0,0 +1,13 @@
+package com.uliahdaraihan_18102034.praktikum8
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class SmsReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("SmsReceiver.onReceive() is not implemented")
+    }
+}
