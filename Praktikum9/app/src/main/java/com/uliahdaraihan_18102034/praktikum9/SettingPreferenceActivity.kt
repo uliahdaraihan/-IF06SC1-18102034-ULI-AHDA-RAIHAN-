41d1ccb405471a8d75b01172d9ca1590419da6ad
Uli Ahda Raihan <18102034@ittelkom-pwt.ac.id>
2020-12-25 19:53:31 +0700

    create SettingPreferenceActivity

---
+++ Praktikum9/app/src/main/java/com/uliahdaraihan_18102034/praktikum9/SettingPreferenceActivity.kt
@@ -0,0 +1,11 @@
+package com.uliahdaraihan_18102034.praktikum9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingPreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_preference)
+    }
+}
