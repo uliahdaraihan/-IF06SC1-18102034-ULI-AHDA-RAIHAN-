aeff57ca41b4b949682ee6c2aae169f5178baad4
Uli Ahda Raihan <18102034@ittelkom-pwt.ac.id>
2020-12-25 16:48:26 +0700

    Add Class SettingModel.kt

--- Praktikum9/app/src/main/java/com/uliahdaraihan_18102034/praktikum9/MainActivity.kt
+++ Praktikum9/app/src/main/java/com/uliahdaraihan_18102034/praktikum9/MainActivity.kt
@@ -2,10 +2,14 @@ package com.uliahdaraihan_18102034.praktikum9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.uliahdaraihan_18102034.praktikum9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = getString(R.string.main_title)
     }
 }
---
+++ Praktikum9/app/src/main/java/com/uliahdaraihan_18102034/praktikum9/data/SettingModel.kt
@@ -0,0 +1,15 @@
+package com.uliahdaraihan_18102034.praktikum9.data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+class SettingModel {
+    @Parcelize
+    data class SettingModel (
+        var name: String? = null,
+        var email: String? = null,
+        var age: Int = 0,
+        var phoneNumber: String? = null,
+        var isDarkTheme: Boolean = false
+    ): Parcelable
+}
