5d00c984ae2ee3ad36af985de42cab2edc7e245e
Uli Ahda Raihan <18102034@ittelkom-pwt.ac.id>
2021-01-01 21:23:31 +0700

    Add Class Quote.kt

---
+++ Praktikum10/app/src/main/java/com/uliahdaraihan_18102034/praktikum10/data/Quote.kt
@@ -0,0 +1,13 @@
+package com.uliahdaraihan_18102034.praktikum10.data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Quote(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var category: String? = null,
+    var date: String? = null
+) : Parcelable
