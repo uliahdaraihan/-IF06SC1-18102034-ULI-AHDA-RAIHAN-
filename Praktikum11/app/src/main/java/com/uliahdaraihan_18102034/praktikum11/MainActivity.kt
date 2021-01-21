123e68f14281e167fe69c43569663722e5fc561a
Uli Ahda Raihan <18102034@ittelkom-pwt.ac.id>
2021-01-21 08:02:22 +0700

    finish 12.2 Konfigurasi Firestore & Halaman Quotes

--- Praktikum11/app/src/main/java/com/uliahdaraihan_18102034/praktikum11/MainActivity.kt
+++ Praktikum11/app/src/main/java/com/uliahdaraihan_18102034/praktikum11/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         googleSignInClient = GoogleSignIn.getClient(this, gso)
         binding.btnSignOut.setOnClickListener(this)
         binding.btnEmailVerify.setOnClickListener(this)
+        binding.btnDashboardQuote.setOnClickListener(this)
 
         val currentUser = auth.currentUser
         if (currentUser == null) {
@@ -66,6 +67,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
             R.id.btnEmailVerify -> {
                 sendEmailVerification()
+            }
+            R.id.btnDashboardQuote -> {
+                val intent = Intent(this@MainActivity, DashboardQuoteActivity::class.java)
+                startActivity(intent)
             }
         }
     }
