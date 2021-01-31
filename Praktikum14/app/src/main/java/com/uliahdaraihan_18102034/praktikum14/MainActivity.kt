96c20a62ef9b953bb456e1cb42c35d58e93c2fa0
Uli Ahda Raihan <18102034@ittelkom-pwt.ac.id>
2021-01-31 19:42:02 +0700

    Finish 14.6 Login

--- Praktikum14/app/src/main/java/com/uliahdaraihan_18102034/praktikum14/MainActivity.kt
+++ Praktikum14/app/src/main/java/com/uliahdaraihan_18102034/praktikum14/MainActivity.kt
@@ -3,6 +3,8 @@ package com.uliahdaraihan_18102034.praktikum14
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -35,5 +37,20 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.topbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_logout -> {
+                tokenPref.removeToken()
+                val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- Praktikum14/app/src/main/java/com/uliahdaraihan_18102034/praktikum14/ui/login/LoginActivity.kt
+++ Praktikum14/app/src/main/java/com/uliahdaraihan_18102034/praktikum14/ui/login/LoginActivity.kt
@@ -1,12 +1,59 @@
 package com.uliahdaraihan_18102034.praktikum14.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import com.uliahdaraihan_18102034.praktikum14.CoroutineContextProvider
+import com.uliahdaraihan_18102034.praktikum14.MainActivity
 import com.uliahdaraihan_18102034.praktikum14.R
+import com.uliahdaraihan_18102034.praktikum14.TokenPref
+import com.uliahdaraihan_18102034.praktikum14.`interface`.MainView
+import com.uliahdaraihan_18102034.praktikum14.api.MainPresenter
+import com.uliahdaraihan_18102034.praktikum14.databinding.ActivityLoginBinding
+import com.uliahdaraihan_18102034.praktikum14.model.Login
+import com.uliahdaraihan_18102034.praktikum14.model.Quote
+import com.uliahdaraihan_18102034.praktikum14.model.Token
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener, MainView {
+    private lateinit var presenter: MainPresenter
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var tokenPref: TokenPref
+    private lateinit var token: Token
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnSign.setOnClickListener(this)
+        presenter = MainPresenter(this, CoroutineContextProvider())
+        tokenPref = TokenPref(this)
+        token = tokenPref.getToken()
     }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnSign -> {
+                presenter.login(
+                    binding.inputNim.text.toString(),
+                    binding.inputPassword.text.toString()
+                )
+            }
+        }
+    }
+
+    override fun showMessage(messsage: String) {
+        Toast.makeText(this, messsage, Toast.LENGTH_SHORT).show()
+    }
+    override fun resultQuote(data: ArrayList<Quote>) {
+    }
+    override fun resultLogin(data: Login) {
+        if (!TextUtils.isEmpty(data.token)) {
+            token.token = data.token
+            tokenPref.setToken(token)
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        } }
 }
