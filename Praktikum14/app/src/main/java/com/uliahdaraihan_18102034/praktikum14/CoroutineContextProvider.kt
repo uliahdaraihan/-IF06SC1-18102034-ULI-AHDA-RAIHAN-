a1f8eb80f6d355379c0c7f8b1d65642ff469df90
Uli Ahda Raihan <18102034@ittelkom-pwt.ac.id>
2021-01-31 19:15:11 +0700

    Finish 14.5 Konfigurasi API Client

---
+++ Praktikum14/app/src/main/java/com/uliahdaraihan_18102034/praktikum14/CoroutineContextProvider.kt
@@ -0,0 +1,7 @@
+package com.uliahdaraihan_18102034.praktikum14
+
+import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
+
+open class CoroutineContextProvider {
+    open val main: CoroutineContext by lazy { Dispatchers.Main } }
---
+++ Praktikum14/app/src/main/java/com/uliahdaraihan_18102034/praktikum14/model/Login.kt
@@ -0,0 +1,10 @@
+package com.uliahdaraihan_18102034.praktikum14.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Login(
+    @SerializedName("token")
+    var token: String? = null,
+    @SerializedName("message")
+    var message: String? = null
+)
